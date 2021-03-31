0fe85a441e6ecf2c91af7cd0f892a20b50ff4b2f
mikekang47 <gyeong0308@gmail.com>
2021-03-31 15:29:57 +0900

    Basic input of get line

--- IOPractice/IOPractice/main.cpp
+++ IOPractice/IOPractice/main.cpp
@@ -4,10 +4,12 @@
 using namespace std;
 
 int main() {
-    int i;
-    cout << "Please enter an integer value: ";
-    cin >> i;
-    cout << "The value you entered is " << i;
-    cout << " and its double is " << i*2 << ".\n";
+    string mystr;
+    cout << "What's your name? ";
+    getline (cin, mystr);
+    cout << "Hello " << mystr << "\n";
+    cout << "What is your favorite team? ";
+    getline (cin, mystr);
+    cout<< "I like " << mystr << " too!\n";
     return 0;
 }
