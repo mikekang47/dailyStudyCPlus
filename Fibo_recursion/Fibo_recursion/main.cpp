718918d442e9a8ecd5f53a3f932b95fb4db00984
mikekang47 <gyeong0308@gmail.com>
2021-04-18 14:44:38 +0900

    Implmented fibonacci by C++

---
+++ Fibo_recursion/Fibo_recursion/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int fibo(int x) {
+    if(x == 1 || x == 2) {
+        return 1;
+    }
+    
+    return fibo(x-1) + fibo(x-2);
+}
+
+int main() {
+    cout << fibo(4) << endl;
+    return 0;
+}
