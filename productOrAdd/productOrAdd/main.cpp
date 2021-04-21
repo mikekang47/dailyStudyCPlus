e11320f428b3990442f50e9c63cf32238493f57a
mikekang47 <gyeong0308@gmail.com>
2021-04-21 15:57:07 +0900

    그리디 문제 해결 더하기 혹은 곱하기

---
+++ productOrAdd/productOrAdd/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sys/cdefs.h>
+
+using namespace std;
+
+string s;
+
+int main() {
+    cin >> s;
+    
+    long long result = s[0] - '0';
+    
+    for(int i = 1; i < s.size(); i++) {
+        int num = s[i] - '0';
+        if(num <= 1 || result <= 1)
+            result += num;
+        else
+            result *= num;
+        
+    }
+    cout << result << '\n';
+}
