7371e54314585caa88544ec9eeb9d5ace34397d0
mikekang47 <gyeong0308@gmail.com>
2021-05-19 23:51:59 +0900

    WIP in mostBiggestNumber in programmers

--- zero/zero/main.cpp
+++ zero/zero/main.cpp
@@ -8,6 +8,12 @@ using namespace std;
 stack<int> st;
 vector<int> v;
 
+/**
+ input : 첫 번째 줄에 정수 k, 이후 k개의 줄까지 정수 입력
+ 입력이 0일 경우 st.top()을 제거.
+ output : 스택 원소 합 출력.
+ */
+
 int main() {
     int k;
     cin >> k;
