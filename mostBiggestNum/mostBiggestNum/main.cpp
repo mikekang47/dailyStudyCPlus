2075199e4994c6a76dadb266fd168f51d2cf521a
mikekang47 <gyeong0308@gmail.com>
2021-05-30 13:22:54 +0900

    Solve 짝지어 제거하기

--- mostBiggestNum/mostBiggestNum/main.cpp
+++ mostBiggestNum/mostBiggestNum/main.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 using namespace std;
  
-bool cmp(string a, string b) {
+bool cmp(string &a, string &b) {
     return a + b > b + a;
 }
  
@@ -13,7 +13,7 @@ string solution(vector<int> numbers) {
     for (auto num : numbers)
         temp.push_back(to_string(num));
     sort(temp.begin(), temp.end(), cmp);
-    if (temp.at(0) == "0")
+    if (temp[0] == "0")
         return "0";
     for (auto num : temp)
         answer += num;
