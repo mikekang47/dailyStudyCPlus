a58c152f430819aa0d5be435f194a43d1c4734af
mikekang47 <gyeong0308@gmail.com>
2021-06-06 23:21:50 +0900

    Implemnted selection sort by index

--- selectionSort/selectionSort/main.cpp
+++ selectionSort/selectionSort/main.cpp
@@ -6,13 +6,12 @@ using namespace std;
 int main() {
     int arr[10] = {1,3,2,5,4,6,7,8,9, 10};
     for(int i = 0; i < 10; i++) {
-        int min = arr[i];
+        int min = i;
         for(int j= i+1; j < 9; j++) {
-            if(arr[j] < min) {
-                min = arr[j];
-                swap(arr[i], arr[j]);
+            if(arr[j] < arr[min]) {
+                min = j;
             }
-           
+            swap(arr[i], arr[min]);
         }
         cout << arr[i] << ' ';
     }
