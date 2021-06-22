f3b07def90295cb6040030f4b4e2047b294c10dc
mikekang47 <gyeong0308@gmail.com>
2021-06-22 18:07:00 +0900

    Sovle printer queue in baekjoon

--- printerQueue/printerQueue/main.cpp
+++ printerQueue/printerQueue/main.cpp
@@ -1,12 +1,45 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
-priority_queue<int, greater<int>()> pq;
-
 int main() {
-    int test;
-    cin >> test;
+    int answer = 0;
+    int t;
+    scanf("%d", &t);
     
+    for(int i = 0; i < t; i++) {
+        answer = 0;
+        int n;
+        int location;
+        int importance;
+        scanf("%d %d", &n, &location);
+
+        queue<pair<int, int>> q;
+        priority_queue<int> pq;
+        
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &importance);
+            q.push({j, importance});
+            pq.push(importance);
+        }
+        
+        while(!q.empty()) {
+            int k = q.front().first;
+            int p = q.front().second;
+            q.pop();
+            
+            if(p == pq.top()) {
+                pq.pop();
+                answer++;
+                if(k == location) {
+                    cout << answer << endl;
+                    break;
+                }
+            } else {
+                q.push(make_pair(k, p));
+            }
+        }
+    }
 }
