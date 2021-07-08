73432fe9a11de88322601d00a804760973c7ac3b
mikekang47 <gyeong0308@gmail.com>
2021-07-08 21:34:33 +0900

    Solve 팩토리얼 0의 개수 in baekjoon

---
+++ numberOfZero/numberOfZero/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int t1 = n / 5;
+    int t2 = n / 25;
+    int t3 = n / 125;
+    cout << t1 + t2 +t3;
+}
