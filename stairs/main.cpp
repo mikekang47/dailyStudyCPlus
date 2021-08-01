3e7910ef85014e7c1d641fda079f4d73453758bb
mikekang47 <gyeong0308@gmail.com>
2021-08-01 21:34:19 +0900

    Solve 계단오르기 in baekjoon

--- stairs/main.cpp
+++ stairs/main.cpp
@@ -10,9 +10,12 @@ int main() {
     for(int i = 0; i < t; i++) {
         cin >> arr[i];
     }
-    for(int i = 0; i <= t-2; i++) {
-        dp[i+2] = max(dp[i+1], dp[i] + arr[i+1]);
+    dp[0] = arr[0];
+    dp[1] = max(arr[1], arr[0] + arr[1]);
+    dp[2] = max(arr[0] + arr[2], arr[1] + arr[2]);
+    for(int i = 3; i < t; i++) {
+        dp[i] = max(dp[i-2] + arr[i], arr[i] + arr[i-1] + dp[i-3]);
     }
-    cout << dp[t] << "\n";
+    cout << dp[t-1] << "\n";
     return 0;
 }
