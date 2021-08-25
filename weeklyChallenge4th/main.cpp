ff3ea4297c55ed51c71d55e6867158d5558dbdcf
mikekang47 <gyeong0308@gmail.com>
2021-08-25 12:36:48 +0900

    Solve weeklyChallenge 4th task

--- weeklyChallenge4th/main.cpp
+++ weeklyChallenge4th/main.cpp
@@ -1,32 +1,53 @@
 #include <string>
 #include <vector>
+#include <queue>
 #include <iostream>
 
 using namespace std;
 
+
 string solution(vector<string> table, vector<string> languages, vector<int> preference) {
     string answer = "";
-    for(int i = 0; i < languages.size(); i++) {
-        for(int j = 0; j < table.size(); j++) {
-            string temp = "";
-            int sum = 0;
-            int point = 6;
-            for(int k = 0; k < table[j].size(); k++) {
-                if(table[j][k] != ' ') {
-                    temp += table[j][k];
-                } else {
-                    if(temp == languages[i]) {
-                        sum += preference[i] * point;
-                        point--;
-                        temp ="";
-                    } else {
-                        point--;
-                        temp = "";
+    priority_queue<pair<int, string>> pq;
+    for(int i = 0; i < table.size(); i++) {
+        string temp = "";
+        int point = 6;
+        int sum = 0;
+        string key;
+        for(int j = 0; j < table[i].size(); j++) {
+            if(table[i][j] != ' ')
+                temp += table[i][j];
+            else if(temp == "SI" || temp == "CONTENTS" || temp == "HARDWARE" || temp == "PORTAL" || temp == "GAME") {
+                key = temp;
+            }
+            if(j == table[i].size()-1 || table[i][j] == ' ') {
+                for(int k = 0; k < languages.size(); k++) {
+                    if(temp == languages[k]) {
+                        sum += preference[k] * point;
                     }
                 }
+                temp = "";
+                point--;
             }
-            cout << sum << "\n";
         }
+        if(!pq.empty() && pq.top().first == sum) {
+            string s = pq.top().second;
+            if(s[0] > key[0]) {
+                pq.pop();
+                pq.push(make_pair(sum, key));
+            }
+        } else {
+            pq.push(make_pair(sum, key));
+        }
+
     }
-    return answer;
+    return pq.top().second;
+
+}
+
+int main() {
+    vector<string> table = {"SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++", "HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP", "GAME C++ C# JAVASCRIPT C JAVA"};
+    vector<string> languages = {"PYTHON", "C++", "SQL"};
+    vector<int> preference = {7, 5, 5};
+    cout << solution(table, languages, preference);
 }
