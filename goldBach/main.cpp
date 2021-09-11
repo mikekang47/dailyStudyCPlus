8960e3b663482215f69bc108089c98d398d1a9df
mikekang47 <gyeong0308@gmail.com>
2021-09-12 00:20:12 +0900

    Solve 골드하브의 추축

--- goldBach/main.cpp
+++ goldBach/main.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
-#include <vector>
 #include <cmath>
-
 using namespace std;
 
-bool prime(int a) {
-    for(int i = 2; i < pow(2,sqrt(a)+1); i++) {
-        if(a % i == 0) {
-            return false;
+bool primeNum(int num){
+    int i = 2,root;
+    root = sqrt(num);
+    if(root == 1)
+        return true;
+    if(num%2){
+        for(;i<=root;i++){
+            if(!(num%i))
+                return false;
+            if(i == root)
+                return true;
         }
     }
-    return true;
+    return false;
 }
-
-int main() {
-    int t;
-    while(t--) {
-        int n;
-        cin >> n;
-        
+int main(){
+    int T,i;
+    cin>>T;
+    for(;i++<T;){
+        int n, a, b, half;
+        cin>>n;
+        half = n/2;
+        for(int j=half; j>=2; j--){
+            a = j;
+            b = n-a;
+            if(primeNum(a) && primeNum(b)){
+                cout<<a<<" "<<b<<'\n';
+                break;
+            }
+        }
     }
-    return 0;
 }
